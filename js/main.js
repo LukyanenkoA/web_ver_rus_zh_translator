e45ce0b96a4dad5c5967964d378bee9038d46263
Shoky13 <alla-alla-luk@yandex.ru>
2023-09-26 20:50:47 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-//fetch('http://127.0.0.1:8000/')
-  //      .then(res => res.json())
-    //    .then(data => {console.log(data)})
 function translate(){
-    let info = document.getElementById('input').value;
+    let info = document.getElementById('text-input').value;
     document.getElementById('output').value = info;
 }
 window.addEventListener("DOMContentLoaded", function (event) {
