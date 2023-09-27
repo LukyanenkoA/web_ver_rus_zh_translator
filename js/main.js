da3915b9207423cc45bb5239e9c54fc005672fcf
Shoky13 <alla-alla-luk@yandex.ru>
2023-09-27 22:03:56 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 function translate(){
-    let info = document.getElementById('text-input').value;
-    document.getElementById('output').value = info;
+    if(document.getElementById('tab-btn-1').checked) {
+        //text input radio button is checked
+        let info = document.getElementById('text-input').value;
+        document.getElementById('output').value = info;
+      }else if(document.getElementById('tab-btn-2').checked) {
+        //draw input radio button is checked
+        let info = document.getElementById('drawkanji-canvas').value;
+        document.getElementById('output').value = 'перевод нарисованного иероглифа';
+      }
 }
 window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
